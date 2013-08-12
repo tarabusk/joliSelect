f929f52ec57f79244b7a0e07f44107082804896f
tarabusk <tarabusk@gmail.com>
2013-08-12 15:27:27 +0200

    Update joliSelect.js

--- joliSelect.js
+++ joliSelect.js
@@ -4,9 +4,8 @@
  *	Copyright (c) 2013 Gaëlle Vaudaine (http://tarabusk.net)
  *	Dual licensed under the MIT (MIT-LICENSE.txt)
  *	and GPL (GPL-LICENSE.txt) licenses.
+ * INSTRUCTIONS : http://memo-web.fr/categorie-jquery-224.php
  *
- *	Built for jQuery library
- *	http://jquery.com
  *
  */
  /*
@@ -37,7 +36,7 @@
     
     $.fn.joliSelect=function(options)
     {     
-	     if ($.browser.msie && (parseInt($.browser.version) < 7))  exit;
+	    // if ($.browser.msie && (parseInt($.browser.version) < 7))  exit;
             //On définit nos paramètres par défaut
             var defauts=
             {      
@@ -58,39 +57,24 @@
             
             return this.each(function()
         {     
-            var element         = $(this);
-			var nom_base        ='_'+element.attr('id');			
+            var element         = $(this);	
+            var id_old          = element.attr('id');			
+            if (element.attr('name')!= 'undefined'){
+			  var name_old          =id_old;	
+			}else{
+			  var name_old          =element.attr('name');	
+			}				
             var totalItems      = element.find('option').length;
             var availableTags   = '[';           
 			var joli_val        = new Array ();
 			var joli_txt        = new Array ();			
 			var largeur_element = element.css('width');
 			
-			/*var labelAss=element.prev("label");
-			if (labelAss.attr('for')==element.attr('id')){
-			  labelAss.attr('for', 'joliSelect'+nom_base);
-			}*/
-			
-			var objet_conteneur = $('<div class="joliSelect" id="joliSelect'+nom_base+'"></div>');
-			objet_conteneur.insertAfter (element);
-			objet_conteneur.css("margin", element.css("margin") );
-			
-			var objet_joli_txt=$( "<input type='text' autocomplete='off' class='joli_txt' name='joli_txt"+nom_base+"' id='joli_txt"+nom_base+"' />" );
-			objet_conteneur.append (objet_joli_txt);
-			
-			
-			var obj_combo_fleche = $( "<b id='combo_fleche"+nom_base+"' class='combo_fleche'> </b>" );
-            obj_combo_fleche.insertAfter (objet_joli_txt);
-			
-			
-			var objet_joli_val=$( "<input type='hidden' name='joli_val"+nom_base+"' id='joli_val"+nom_base+"'  />" );
-			objet_joli_val.insertAfter (obj_combo_fleche);
-			
 			var html = '';  				
-			html += '<ul id="combo'+nom_base+'">';			
+			html += '<ul id="combo_'+id_old+'">';			
            
-		    joli_txt[nom_base] = parametres.defaultText;
-			joli_val[nom_base] = -1;
+		    joli_txt[id_old] = parametres.defaultText;
+			joli_val[id_old] = -1;
 					
             element.find('option').each(function(i) //Puis on parcourt chaque item !
             {	
@@ -102,24 +86,46 @@
 			    var txtShow = tabTxt[0];
 			 
               var classe_selected='';  			
-			  if ($(this).attr('selected')){			     
-			    joli_txt[nom_base]  = txtShow;				
+			  if ($(this).prop('selected')){			     
+			    joli_txt[id_old]  = txtShow;				
 				if ($(this).val()!='')
-			      joli_val[nom_base]    = $(this).val();
+			      joli_val[id_old]    = $(this).val();
 				else
-				  joli_val[nom_base]    = txtShow;
+				  joli_val[id_old]    = txtShow;
 				classe_selected  = 'item_sel';
 			  }
 			  html += '<li class="'+classe_selected+'">'+txtItem;
-			  html += '<input type="hidden" class="hidden'+nom_base+'" value="'+$(this).val()+'" />';
-			  html += '<input type="hidden" class="txtS'+nom_base+'" value="'+txtShow+'" />';
+			  html += '<input type="hidden" class="hidden_'+id_old+'" value="'+$(this).val()+'" />';
+			  html += '<input type="hidden" class="txtS_'+id_old+'" value="'+txtShow+'" />';
 			  html += ' </li>';	
 			  
 			  availableTags +='{id : "'+$(this).val()+'", label :"'+txtItem+'", labelS :"'+txtShow+'"}';	
 			  if (i+1<totalItems) availableTags+=',';
             });		
 			html += '</ul>';
+						
+			//--> construction du nouvel objet
+			
+			/*var labelAss=element.prev("label");
+			if (labelAss.attr('for')==element.attr('id')){
+			  labelAss.attr('for', 'joliSelect'+id_old);
+			}*/
+			
+			var objet_conteneur = $('<div class="joliSelect" id="joliSelect_'+id_old+'"></div>');
+			objet_conteneur.insertAfter (element);
+			objet_conteneur.css("margin", element.css("margin") );
+			
 			element.remove();
+			
+			var objet_joli_txt=$( "<input type='text' autocomplete='off' class='joli_txt' name='joli_txt_"+id_old+"' id='joli_txt_"+id_old+"' />" );
+			objet_conteneur.append (objet_joli_txt);		
+			
+			var obj_combo_fleche = $( "<b id='combo_fleche_"+id_old+"' class='combo_fleche'> </b>" );
+            obj_combo_fleche.insertAfter (objet_joli_txt);
+			
+			var objet_joli_val=$( "<input type='hidden' name='"+name_old+"' id='"+id_old+"' />" );
+			objet_joli_val.insertAfter (obj_combo_fleche);
+			
 			var objet_combo=$( html );
 			objet_combo.insertAfter (objet_joli_val);
 				
@@ -127,9 +133,9 @@
 			availableTags+=']';
 			availableTags=eval(availableTags);					
           		
-			objet_joli_txt.val(joli_txt[nom_base]);
-			objet_joli_txt.attr('title',joli_txt[nom_base]);
-			objet_joli_val.val(joli_val[nom_base]);       
+			objet_joli_txt.val(joli_txt[id_old]);
+			objet_joli_txt.attr('title',joli_txt[id_old]);
+			objet_joli_val.val(joli_val[id_old]);       
 			
 			if (parametres.width > 0){			 
 			  var largeur_combo = parametres.width;
@@ -165,7 +171,7 @@
 						'marginTop':'2px'
                     });
 			
-			$("#combo"+nom_base+" li").css(
+			$("#combo_"+id_old+" li").css(
                     {					    
 						'padding':'2px',
                         'display': 'block',
@@ -183,24 +189,28 @@
                     });
 			
 				
-			function closeCombo(e){		 		  					
-			  objet_joli_txt.val(joli_txt[nom_base]);
-              objet_joli_txt.attr('title',joli_txt[nom_base]);			  
-			  objet_joli_val.val(joli_val[nom_base]);
+			function closeCombo(){				   
 			  objet_combo.hide('fast');
-			  $("#combo"+nom_base+" li").removeClass("item_sel");          		  
-			  $('input[type=hidden][class=hidden'+nom_base+'][value='+joli_val[nom_base]+']').parent('li').addClass("item_sel");
-              if(parametres.onChooseItem)
-			  {
-				parametres.onChooseItem($(this));
-			  } 			  
+			  objet_joli_txt.val(joli_txt[id_old]);			 
+			  if (objet_joli_val.val() != joli_val[id_old]){			  
+				  if (parametres.onChooseItem){
+					parametres.onChooseItem($(this));
+				  }			  
+				  objet_joli_txt.val(joli_txt[id_old]);
+				  objet_joli_txt.attr('title',joli_txt[id_old]);			  
+				  objet_joli_val.val(joli_val[id_old]);
+				  
+				  $("#combo_"+id_old+" li").removeClass("item_sel");          		  
+				  $('input[type=hidden][class=hidden_'+id_old+'][value='+joli_val[id_old]+']').parent('li').addClass("item_sel");
+              }			  
               //e.stopPropagation(); 	//Viré car pose problème avec l'autocomplétion, ne valide pas le choix. Pb à résoudre !		  
 			}
 			
 			// Affectation des événements sur les objets 		
-			$('html').click(function(e){          		
+			$('html').click(function(e){    
+
 			  if(e.target.id == objet_joli_txt.attr('id') || e.target.id == objet_combo.attr('id') ||e.target.id == obj_combo_fleche.attr('id')) {
-			  } else {
+			  } else {			    
 			    closeCombo(e);		
 			  }
 			});
@@ -210,28 +220,28 @@
 			  minLength: 1,
 			  select: function(event, ui)
 						{		                        					
-                         joli_txt[nom_base] = ui.item.labelS;	
-                         joli_val[nom_base]   = ui.item.id;	
+                         joli_txt[id_old] = ui.item.labelS;	
+                         joli_val[id_old]   = ui.item.id;	
 						 return false;                        						 
 						},
+			  
 			  close: function(event, ui)
 						{							 					 
 						} 				   
 		    });
 			
-			$("#combo"+nom_base+" li").hover(
+			$("#combo_"+id_old+" li").hover(
 			  function () {
-				$(this).css({'background':parametres.bkdColorSelect});
+				$(this).css({'background': parametres.bkdColorSelect});
 			  },
 			  function () {
-				$(this).css({'background':parametres.bkdColor});
+				$(this).css({'background': parametres.bkdColor});
 			  }
 			);
-
 			
-			$("#combo"+nom_base+" li").click(function() {			                
-			    joli_txt[nom_base] = $(this).children('input[type=hidden][class=txtS'+nom_base+']').val();			 
-				joli_val[nom_base]   = $(this).children('input').val();               				
+			$("#combo_"+id_old+" li").click(function() {			                
+			    joli_txt[id_old] = $(this).children('input[type=hidden][class=txtS_'+id_old+']').val();			 
+				joli_val[id_old]   = $(this).children('input').val();               				
                 $("#select_combo").toggle('fast');   							
             });
 			
@@ -241,7 +251,7 @@
             });
 		
 			
-			objet_combo.click(function(e) {		
+			objet_combo.click(function(e) {			
                 closeCombo(e);
             });
 			
